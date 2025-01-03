b9a99fb752959dc60d072445e712da67b99be20b
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-01-03 15:43:15 -0600

    Helper scripts

---
+++ scripts/extract_condition_harmony.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+import sys
+from csv import DictReader, writer as csv_writer
+from yaml import safe_load
+from argparse import ArgumentParser, FileType
+from pathlib import Path
+
+
+class MissingCurie(Exception):
+    def __init__(self, code):
+        self.code = code
+        super().__ionit__(self.message())
+
+    def message(self):
+        return f"There was no curie associated with the code, {self.code}"
+
+
+class Coding:
+    fhir_system = {
+        "HP": "http://purl.obolibrary.org/obo/hp.owl",
+        "MONDO": "http://purl.obolibrary.org/obo/mondo.owl",
+        "MAXO": "http://purl.obolibrary.org/obo/maxo.owl",
+        "NCIT": "http://purl.obolibrary.org/obo/ncit.owl",
+        "SNOMED": "http://snomed.info/sct",
+        "SYMP": "https://purl.obolibrary.org/obo/symp.owl",
+        "LOINC": "https://loinc.org/",
+        "MEDDRA": "https://www.meddra.org",
+        "MESH": "urn:oid:2.16.840.1.113883.6.177",
+        "UCUM": "http://unitsofmeasure.org",
+        "OMIT": "http://purl.obolibrary.org/obo/omit.owl",
+    }
+
+    def __init__(self, local_code, local_display, parent_varname, code, display):
+        self.local_code = local_code
+        self.local_display = local_display
+        self.parent_varname = parent_varname
+        self.display = display
+        try:
+            self.curie, self.code = code.split(":")
+        except:
+            raise MissingCurie(code)
+
+        self.system = Coding.fhir_system[self.curie]
+
+    def row(self):
+        "local code,text,table_name,parent_varname,local code system,code,display,code system,comment"
+        return [
+            self.local_code,
+            self.local_display,
+            "condition",
+            self.parent_varname,
+            "condition_description",
+            self.code,
+            self.display,
+            self.system,
+            "",
+        ]
+
+
+def ExtractCoding(line, writer, code_colname, display_colname, observed_codes):
+    code = line[code_colname]
+    display = line[display_colname]
+
+    if code != "NA":
+        if code not in observed_codes:
+            coding = Coding(
+                line["Condition or Measure Source Text"],
+                line["Condition or Measure Source Text"],
+                code_colname,
+                code,
+                display,
+            )
+            writer.writerow(coding.row())
+            observed_codes[code] = coding
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Extract harmony details for a based on the condition details described in the configuration file."
+    )
+
+    parser.add_argument(
+        "config",
+        nargs="+",
+        type=FileType("rt"),
+        help="Whistler configuration YAML file for study to be addressed. ",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Filename to write conditions to. By default, it will be harmony/{study_id}-conditions.csv",
+    )
+    args = parser.parse_args()
+
+    for f in args.config:
+        config = safe_load(f)
+
+        study_id = config["study_id"]
+
+        output_filename = args.output
+        if output_filename is None:
+            output_filename = f"harmony/{study_id.lower()}-conditions.csv"
+
+        condition_filename = config["dataset"]["condition"]["filename"]
+        with open(condition_filename, "rt") as cfile:
+            observed_codes = {}
+
+            with open(output_filename, "wt") as outf:
+                writer = csv_writer(outf)
+                writer.writerow(
+                    "local code,text,table_name,parent_varname,local code system,code,display,code system,comment".split(
+                        ","
+                    )
+                )
+
+                # We have a few other condition related entries that we need to prepopulate
+                writer.writerow(
+                    "Observed,Condition was observed or reported (this will be the case for most conditions),condition,Condition Interpretation,Condition Interpretation,LA33635-6,Observed,http://loinc.org".split(
+                        ","
+                    )
+                )
+                writer.writerow(
+                    "Observed,Condition was observed or reported (this will be the case for most conditions),Condition,Condition Interpretation,Condition Interpretation,confirmed,Confirmed,http://terminology.hl7.org/CodeSystem/condition-ver-status".split(
+                        ","
+                    )
+                )
+                writer.writerow(
+                    "Not Observed,Not Observed,condition,Condition Interpretation,Condition Interpretation,LA19655-2,Not Observed,http://loinc.org".split(
+                        ","
+                    )
+                )
+                writer.writerow(
+                    "Current,Condition is ongoing,Condition,Condition Status,Condition Status,LA9040-2,Ongoing,http://loinc.org".split(
+                        ","
+                    )
+                )
+                writer.writerow(
+                    "Resolved,Condition has been resolved,Condition,Condition Status,Condition Status,LA9041-0,Resolved,http://loinc.org".split(
+                        ","
+                    )
+                )
+                writer.writerow(
+                    "History Of,History Of,condition,Condition Status,Condition Status,392521001,Histor of,SMOMED,http://snomed.info/sct".split(
+                        ","
+                    )
+                )
+
+                reader = DictReader(cfile)
+                for line in reader:
+                    ExtractCoding(line, writer, "HPO Code", "HPO Label", observed_codes)
+                    ExtractCoding(
+                        line, writer, "MONDO Code", "MONDO Label", observed_codes
+                    )
+                    ExtractCoding(
+                        line, writer, "MAXO Code", "MAXO Label", observed_codes
+                    )
+                    ExtractCoding(
+                        line, writer, "Other Code", "Other Label", observed_codes
+                    )
---
+++ scripts/read-csv.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""This is just a quick way to review a CSV file in JSONish format so that I can avoid Excel. It will just dump the output from DictReader to stdout..."""
+
+from csv import DictReader
+import sys
+from rich import print
+import os
+from argparse import ArgumentParser, FileType
+
+import pdb
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Quickly dump some or all of a CSV file in a JSON like, readable format."
+    )
+
+    parser.add_argument(
+        "file", nargs="+", type=FileType("rt"), help="CSV file to be inspected. "
+    )
+
+    parser.add_argument("-i", "--index", type=int, action="append")
+    parser.add_argument("-c", "--row-count", type=int, default=-1)
+    args = parser.parse_args()
+
+    if len(args.index) == 0:
+        # We'll assume no index means just start at the beginning
+        args.index = set([1])
+    else:
+        args.index = set(args.index)
+
+    for file in args.file:
+        try:
+            reader = DictReader(file)
+
+            rows_shown = 0
+            row_count = 1
+            row_index = 1
+
+            for row in reader:
+                row_index += 1
+                if row_index in args.index:
+                    row_count = args.row_count
+                    rows_shown = 0
+                if row_count < 1 or rows_shown < row_count:
+                    print(row)
+                    rows_shown += 1
+        except:
+            os.system(f"file --mime-encoding {file.name}")
+            print()
+            print(f"There was a problem reading the file, {file.name}")
